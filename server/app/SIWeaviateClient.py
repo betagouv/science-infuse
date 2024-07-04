2af82ecc6ec119d6affcf9d9bf576f51c4db43fc
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-04 20:47:27 +0200

    update docker compose

--- server/app/SIWeaviateClient.py
+++ server/app/SIWeaviateClient.py
@@ -8,7 +8,7 @@ class SIWeaviateClient:
     def connect_to_local(self) -> WeaviateClient:
         return weaviate.connect_to_local()
 
-    def connect_to_docker(self) -> WeaviateClient:
+    def connect_with_env(self) -> WeaviateClient:
         return weaviate.connect_to_custom(
             http_host=os.getenv("WEAVIATE_URL"),
             http_port=os.getenv("WEAVIATE_PORT"),
@@ -21,7 +21,8 @@ class SIWeaviateClient:
 
     def __enter__(self) -> WeaviateClient:
         # Change the connection function here
-        self.client = self.connect_to_local()
+        # self.client = self.connect_to_local()
+        self.client = self.connect_with_env()
         return self.client
 
     def __exit__(self, exc_type, exc_val, exc_tb):
