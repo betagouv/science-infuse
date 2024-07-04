8baad63e747630cb5b597b76a8c2c272f8183d7f
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-04 21:49:39 +0200

    update docker compose

--- webapp/src/config.ts
+++ webapp/src/config.ts
@@ -1,2 +1,3 @@
 export const NEXT_PUBLIC_FILE_SERVER_URL = process.env.NEXT_PUBLIC_FILE_SERVER_URL;
 export const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+console.log("NEXT_PUBLIC_SERVER_URL process env", process.env)
