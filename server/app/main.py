70df2b04de629a852aa9bb6078e30798819886f2
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-04 22:01:03 +0200

    update envs

--- server/app/main.py
+++ server/app/main.py
@@ -23,14 +23,11 @@ async def catch_exceptions_middleware(request: Request, call_next):
 
 app = FastAPI()
 
-origins = [
-    "*"
-    # "http://localhost:3000",
-    # "http://localhost:8002",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
