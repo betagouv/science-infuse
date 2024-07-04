c1d5ad19af051314c9bd30288b9f664f8c871c86
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-04 20:53:36 +0200

    update docker compose

--- server/app/SIWeaviateClient.py
+++ server/app/SIWeaviateClient.py
@@ -9,11 +9,19 @@ class SIWeaviateClient:
         return weaviate.connect_to_local()
 
     def connect_with_env(self) -> WeaviateClient:
+        print("WEAVIATE_URL", os.getenv("WEAVIATE_URL"))
+        print("WEAVIATE_PORT", os.getenv("WEAVIATE_PORT"))
+        print("WEAVIATE_URL", os.getenv("WEAVIATE_URL"))
+        print("WEAVIATE_GRPC_PORT", os.getenv("WEAVIATE_GRPC_PORT"))
         return weaviate.connect_to_custom(
             http_host=os.getenv("WEAVIATE_URL"),
-            http_port=os.getenv("WEAVIATE_PORT"),
+            http_port=int(os.getenv("WEAVIATE_PORT")),
             http_secure=False,
+            grpc_host=os.getenv("WEAVIATE_URL"),
+            grpc_port=int(os.getenv("WEAVIATE_GRPC_PORT")),
+            grpc_secure=False,
         )
+
 
     # TODO: implement different connection strategies for prod/...
     # def connect_to_remote(self):
