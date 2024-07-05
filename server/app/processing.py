87f5a591ac0f475713fddf7a307705a91842c3ea
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-05 20:29:37 +0200

    singleton for models

--- server/app/processing.py
+++ server/app/processing.py
@@ -6,6 +6,6 @@ import time
 from ftp_file_watcher import main
 
 path_to_watch = os.path.join(os.getcwd(), 'ftp-data')
-print(f"Watching directory: {path_to_watch}")
+print(f"Watching directory: {path_to_watch}", flush=True)
     
 asyncio.run(main(path_to_watch))
