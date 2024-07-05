40a11cfbfdd2eb4e3261e742acdcebcff257600a
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-05 16:08:30 +0200

    update to take NNNe Comité du

--- server/app/document_processor.py
+++ server/app/document_processor.py
@@ -7,6 +7,9 @@ image_descriptor = SIImageDescription()
 translator = SITranslator()
 
 def process_pdf(pdf_path: str):
-    with SIWeaviateClient() as client:
-        pdf = PDFProcessor(client, image_descriptor, translator, pdf_path)
-    return pdf
+    try:
+        with SIWeaviateClient() as client:
+            pdf = PDFProcessor(client, image_descriptor, translator, pdf_path)
+        return pdf
+    except Exception as e:
+        print(f"PDFProcessor : An error occurred", e)
