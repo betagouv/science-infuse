be754bf16f917f2c662809aaed10189562388c5a
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-08 18:15:26 +0200

    parsing QUESTIONS-SANTÉ

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-TODO
+## Docker compose services
+### weaviate
+Weaviate is an open source vector search engine that allows you to store data objects and query them by semantic meaning. It uses machine learning models to create vector embeddings of your data and can find similar objects based on their vector representations.
+
+### t2v-custom
+text-to-vec, custom embeddings model (solon) to create vector embeddings of the text of the database
+
+
+### weaviate-console
+Run weaviate console to query data using GraphQL (debugging purpose)
+
+### nginx
+used as an efficient way to serve stored files in folder documents (raw documents indexed by weaviate)
+
+### webapp
+A NextJS client to query the database
+
+### server *
+A Python3 / FastAPI server to handle database queries
+
+
+### ftp_processing *
+A Python3 script that listens to file writes in `ftp-data` (see ftp section), and index them using Machine Learning technics.
+
+### ftp
+A ftp server that write files to `ftp-data`
+
+**\*** These services use the same Docker image, but with different entry points.
+
---
+++ server/data_indexing/youtube_videos.py
@@ -0,0 +1,49 @@
+from googleapiclient.discovery import build
+
+# Replace with your own API key
+api_key = "AIzaSyC1f_efS0lVjel843u4_JJiWOH6AIFvOEk"
+
+# Create a YouTube API client
+youtube = build("youtube", "v3", developerKey=api_key)
+
+# Replace with the channel ID you want to fetch videos from
+# LE BLOB
+
+def get_channel_videos(channel_id):
+    videos = []
+    next_page_token = None
+    
+    while True:
+        request = youtube.search().list(
+            part="snippet",
+            channelId=channel_id,
+            maxResults=50,
+            type="video",
+            pageToken=next_page_token
+        )
+        response = request.execute()
+        
+        for item in response["items"]:
+            videos.append({
+                "title": item["snippet"]["title"],
+                "video_id": item["id"]["videoId"],
+                "publish_time": item["snippet"]["publishTime"]
+            })
+        
+        next_page_token = response.get("nextPageToken")
+        break        
+        if not next_page_token:
+            break
+    
+    return videos
+
+# Fetch the videos
+channel_id = "UC3E2DhYIqnoc6H3WXwTVnlA"
+channel_videos = get_channel_videos(channel_id)
+
+# Print the results
+for video in channel_videos:
+    print(f"Title: {video['title']}")
+    print(f"Video ID: {video['video_id']}")
+    print(f"Publish Time: {video['publish_time']}")
+    print("---")
