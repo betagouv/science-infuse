6c3da8a3059beec5f9ee4e1b3cf821f1cc13d4ae
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-10 13:07:18 +0200

    add s3storage class to upload file + update requirements to add libcudnn

--- server/Readme.md
+++ server/Readme.md
@@ -14,3 +14,10 @@ python app/main.py
 ## index data
 ### index cite-sante
 python app/data_indexing/health_questions.py
+
+
+### NOTES
+Need to use S3 for file storage, but s3 does not provide direct link to see access file.
+Need to use `s3_client.generate_presigned_url` with `Key={OBJECT_PATH}` to get a temporary file path
+But we don't want links to expire.
+So do a nginx / or fastapi route that takes the file_path (OBJECT_PATH) (which is the thing we store in the db) and returns the public path (redirect / pipe ?) created by `s3_client.generate_presigned_url`
---
+++ server/app/S3Storage.py
@@ -0,0 +1,60 @@
+import boto3
+from botocore.client import Config
+import os
+
+class S3Storage:
+    def __init__(self):
+        self.S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY", "")
+        self.S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY", "")
+        self.S3_ENDPOINT = os.environ.get("S3_ENDPOINT", "")
+        self.S3_REGION = os.environ.get("S3_REGION", "")
+        self.bucket_name = os.environ.get("S3_BUCKET", "")
+        
+        self.s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=self.S3_ACCESS_KEY,
+            aws_secret_access_key=self.S3_SECRET_KEY,
+            endpoint_url=self.S3_ENDPOINT,
+            region_name=self.S3_REGION
+        )
+
+    def list_buckets(self):
+        response = self.s3_client.list_buckets()
+        print("Buckets existants :")
+        for bucket in response['Buckets']:
+            print(f'  {bucket["Name"]}')
+
+    def list_objects(self):
+        response = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
+        print(f"Objets dans le bucket {self.bucket_name} :")
+        for obj in response.get('Contents', []):
+            print(f'  {obj["Key"]}')
+
+    def upload_file(self, file_path, object_name):
+        try:
+            self.s3_client.upload_file(file_path, self.bucket_name, object_name, ExtraArgs={'ACL': 'public-read'})
+            print(f"File {file_path} uploaded successfully to bucket {self.bucket_name} as {object_name}.")
+            
+            # Generate the public URL
+            public_url = f"{self.S3_ENDPOINT}/{self.bucket_name}/{object_name}"
+            print(f"Public URL: {public_url}")
+            return public_url
+        except Exception as e:
+            print(f"Error during file upload: {e}")
+            return None
+
+    def get_presigned_url(self, object_name, expiration=3600):
+        return self.s3_client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={'Bucket': self.bucket_name, 'Key': object_name},
+            ExpiresIn=expiration  # URL expiration time in seconds
+        )
+    
+    def remove_file(self, object_name):
+        try:
+            self.s3_client.delete_object(Bucket=self.bucket_name, Key=object_name)
+            print(f"File {object_name} successfully removed from bucket {self.bucket_name}.")
+            return True
+        except Exception as e:
+            print(f"Error during file removal: {e}")
+            return False
