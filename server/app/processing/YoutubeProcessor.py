d0728570a60be128e3755c5bc6c8dbc0b847cea5
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-12 20:53:29 +0200

    computePictoFrame fully working with star & end icon & signature & right calculation for dates

--- server/app/processing/YoutubeProcessor.py
+++ server/app/processing/YoutubeProcessor.py
@@ -28,13 +28,13 @@ class YoutubeProcessor(BaseDocumentProcessor):
         # Load and process audio file
         video_path, video_name = self.download_youtube_video()
         video_s3_object_name = f"youtube/{self.get_random_uuid()}.mp4"
-        print("EXTRACT DOCUMNET YOUTUBE video_s3_object_name 1", video_s3_object_name)
+        # print("EXTRACT DOCUMNET YOUTUBE video_s3_object_name 1", video_s3_object_name)
         # save video to s3
         self.whisper.set_paragraph_pause_threshold(self.paragraph_pause_threshold)
         segments = self.whisper.get_paragraphs_from_audio_path(video_path)
         
         self.save_to_s3(self.s3, video_path, video_s3_object_name)
-        print("EXTRACT DOCUMNET YOUTUBE video_s3_object_name 2", video_s3_object_name)
+        # print("EXTRACT DOCUMNET YOUTUBE video_s3_object_name 2", video_s3_object_name)
 
         document = Document(
             document_id=self.id, 
