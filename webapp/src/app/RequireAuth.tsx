1b6a3fbf9a1515caff68f3406de810bc892ab112
Erwan BOEHM <erwan.boehm@gmail.com>
2024-07-17 14:43:20 +0200

    fix custom tiptap extensions typescript

--- webapp/src/app/RequireAuth.tsx
+++ webapp/src/app/RequireAuth.tsx
@@ -2,8 +2,8 @@ import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import React from "react"
 
-export function requireAuth(Component) {
-  return function AuthenticatedComponent(props) {
+export function requireAuth(Component: React.ComponentType<any>) {
+  return function AuthenticatedComponent(props: any) {
     const { data: session, status } = useSession()
     const router = useRouter()
 
--- webapp/src/course_editor/extensions/ImageSearch/index.tsx
+++ webapp/src/course_editor/extensions/ImageSearch/index.tsx
@@ -1,67 +1,71 @@
 import { Extension } from '@tiptap/core'
-import { Editor, ReactRenderer } from '@tiptap/react'
+import { Command, Editor, RawCommands, ReactRenderer } from '@tiptap/react'
 import tippy from 'tippy.js'
 import ImageSearchPopup from './ImageSearchPopup'
 
+declare module "@tiptap/core" {
+    interface Commands<ReturnType> {
+      ImageSearch: {
+        openImageSearch: () => ReturnType;
+      };
+    }
+  }
+
+  
 export const ImageSearchExtension = Extension.create({
     name: 'imageSearch',
 
-    // @ts-ignore
     addCommands() {
         return {
-            openImageSearch: () => (props: { editor: Editor }) => {
-                const { editor } = props;
-                let tippyInstance: any;
+            openImageSearch:
+                () =>
+                    ({ editor, chain }) => {
+                        let tippyInstance: any;
 
-                const component = new ReactRenderer(ImageSearchPopup, {
-                    props: {
-                        editor,
-                        closePopup: () => {
-                            if (tippyInstance) {
-                                console.log("TIPPYINSTANCE", tippyInstance.popper)
-                                tippyInstance.popper.querySelector('.tippy-box').style.opacity = '0'
+                        const component = new ReactRenderer(ImageSearchPopup, {
+                            props: {
+                                editor,
+                                closePopup: () => {
+                                    if (tippyInstance) {
+                                        tippyInstance.destroy();
+                                    }
+                                },
+                            },
+                            editor,
+                        })
+
+                        tippyInstance = tippy(document.body, {
+                            getReferenceClientRect: () => {
+                                const { view } = editor
+                                const { state } = view
+                                const { selection } = state
+                                const { ranges } = selection
+                                const from = Math.min(...ranges.map(range => range.$from.pos))
+                                const to = Math.max(...ranges.map(range => range.$to.pos))
+                                const domRect = view.coordsAtPos(from)
+                                const domRectTo = view.coordsAtPos(to)
+                                return new DOMRect(
+                                    domRect.left,
+                                    domRect.top,
+                                    domRectTo.right - domRect.left,
+                                    domRectTo.bottom - domRect.top
+                                )
+                            },
+                            appendTo: () => document.body,
+                            content: component.element,
+                            onCreate(instance) {
                                 setTimeout(() => {
-                                    console.log("TIPPYINSTANCE DESTROY", tippyInstance)
-                                    tippyInstance.destroy();
+                                    instance.popper.querySelector("input")?.focus()
                                 }, 400)
-                            }
-                        },
-                    },
-                    editor,
-                });
+                            },
+                            showOnCreate: true,
+                            interactive: true,
+                            trigger: 'manual',
+                            placement: 'bottom-start',
+                        })
 
-                tippyInstance = tippy(document.body, {
-                    getReferenceClientRect: () => {
-                        const { view } = editor
-                        const { state } = view
-                        const { selection } = state
-                        const { ranges } = selection
-                        const from = Math.min(...ranges.map(range => range.$from.pos))
-                        const to = Math.max(...ranges.map(range => range.$to.pos))
-                        const domRect = view.coordsAtPos(from)
-                        const domRectTo = view.coordsAtPos(to)
-                        return new DOMRect(
-                            domRect.left,
-                            domRect.top,
-                            domRectTo.right - domRect.left,
-                            domRectTo.bottom - domRect.top
-                        )
+                        return chain().run();
                     },
-                    appendTo: () => document.body,
-                    content: component.element,
-                    onCreate(instance) {
-                        setTimeout(() => {
-                            instance.popper.querySelector("input")?.focus()
-                        }, 0)
-                    },
-                    showOnCreate: true,
-                    interactive: true,
-                    trigger: 'manual',
-                    placement: 'bottom-start',
-                })
-
-                return true
-            },
         }
     },
 })
