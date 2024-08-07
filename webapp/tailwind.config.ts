79f3ff9eeec7a27566def958144215ea602325a3
Erwan BOEHM <erwan.boehm@gmail.com>
2024-08-07 16:23:41 +0200

    add ai quiz generation and text completion

--- webapp/tailwind.config.ts
+++ webapp/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./css/app/css/*.{css}"
   ],
   corePlugins: { 
     preflight: false 
