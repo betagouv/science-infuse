94627d484ff9527cdff4bb89627a6460e598c5db
Erwan BOEHM <erwan.boehm@gmail.com>
2024-08-27 18:07:48 +0200

    fix build issue

--- webapp/src/app/api/keyIdeas/route.ts
+++ webapp/src/app/api/keyIdeas/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
 
-    try {
+    // try {
         const keyIdeas = await prisma.keyIdea.findMany();
         return NextResponse.json(keyIdeas)
-    } catch (error) {
-        console.error('Error reading file:', error);
-        return NextResponse.json({ error: error }, { status: 404 });
-    }
+    // } catch (error) {
+    //     console.error('Error reading file:', error);
+    //     return NextResponse.json({ error: error }, { status: 404 });
+    // }
 }
