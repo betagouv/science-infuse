ff00cd7d7cdff79a345d80cc82a19a25dc9d8fa0
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-09 13:45:33 +0200

    export pdf feature

---
+++ webapp/src/app/api/export/pdf/route.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import puppeteer from 'puppeteer';
+
+export const dynamic = 'force-dynamic'
+export async function POST(request: NextRequest) {
+    const { html } = await request.json();
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    const pdfBuffer = await page.pdf({ format: 'A4' });
+    await browser.close();
+    
+    return new NextResponse(pdfBuffer, {
+        headers: {
+            'Content-Type': 'application/pdf',
+        },
+    });
+}
