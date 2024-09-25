f958bc776314b4bc58680b0ad02346a5d16b9e4a
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-25 19:35:19 +0200

    add cgu and confidentialite

---
+++ webapp/src/app/conditions-generales-d-utilisation/page.tsx
@@ -0,0 +1,119 @@
+const CGU = () => {
+    return (
+        <div className="w-full fr-grid-row fr-grid-row--center">
+            <div className="fr-col-12 fr-col-md-6 main-content-item my-24">
+                <div className="py-16 flex flex-col gap-4 md:px-0">
+
+
+                    <h1>Conditions Générales d'Utilisation (CGU) - Science Infuse</h1>
+
+                    <p>Les présentes conditions générales d’utilisation (dites « CGU ») fixent le cadre juridique de la plateforme Science Infuse (ci-après « la plateforme ») et définissent les conditions d’accès et d’utilisation des services par les Enseignants.</p>
+                    <p>Science Infuse est portée par <a href="https://www.universcience.fr/fr/accueil">Universcience</a> en partenariat avec le ministère de la Culture.</p>
+                    <p>La plateforme Science Infuse est actuellement en phase expérimentale. Les présentes conditions générales d'utilisation (CGU) sont donc susceptibles d'être modifiées et mises à jour régulièrement. En utilisant la plateforme, vous reconnaissez et acceptez ce caractère évolutif et vous engagez à consulter régulièrement les CGU pour prendre connaissance des éventuelles modifications. </p>
+                    <p>Les contenus de cette plateforme proviennent d’<a href="https://www.universcience.fr/fr/accueil">Universcience</a> qui regroupe la Cité des sciences et de l'industrie et le Palais de la découverte depuis janvier 2010. Universcience s'est donné pour mission de rendre les sciences accessibles et attrayantes, tout en promouvant la culture scientifique et technique. Son objectif principal est d'aider les citoyens de tous âges et de tous niveaux à mieux comprendre le monde, en expliquant clairement les principes scientifiques et les avancées technologiques. Universcience s'engage également à encourager la passion des sciences et des technologies chez les jeunes, en vue de les inciter à entreprendre des carrières scientifiques.</p>
+
+                    <h2>Article 1 – Champ d'Application</h2>
+                    <p>La plateforme Science Infuse est un service en cours de développement et est fournie à titre expérimental. En conséquence, Universcience et le ministère de la Culture se réservent le droit de modifier ces CGU à tout moment afin de refléter les évolutions de la plateforme.
+                    </p>
+                    <p>La plateforme est destinée aux enseignants de sciences du secondaire. En créant un compte, les utilisateurs acceptent explicitement les présentes CGU. Universcience et le ministère de la Culture se réservent le droit de modifier ces CGU à tout moment. Les utilisateurs seront informés des mises à jour et devront les accepter pour continuer à utiliser le service.</p>
+                    <p>L'utilisation de la plateforme implique l'acceptation des présentes CGU ainsi que de notre <a href="politique-de-confidentialite.html">Politique de Confidentialité</a>.</p>
+
+                    <h2>Article 2 – Inscription et Accès à la Plateforme</h2>
+                    <h3>2.1 Inscription</h3>
+                    <p>Pour accéder aux services de la plateforme, les enseignants doivent fournir des informations personnelles : prénom, nom, courriel professionnel, mot de passe, académie de rattachement, nom de l’établissement scolaire, niveaux enseignés pour l’année en cours, et la matière enseignée.</p>
+                    <p>La collecte de ces données est nécessaire à la création et à la gestion de votre compte utilisateur, ainsi qu'à l'accès aux fonctionnalités de la plateforme. Pour plus d'informations sur les données collectées et les finalités de traitement, veuillez consulter notre <a href="politique-de-confidentialite.html">Politique de Confidentialité</a>.</p>
+
+                    <h3>2.2 Authentification</h3>
+                    <p>L’accès aux fonctionnalités de la plateforme, telles que le moteur de recherche ou le module de création de cours, nécessite une authentification de l’utilisateur.</p>
+
+                    <h3>2.3 Conservation des Données</h3>
+                    <p>Les données de contact, données professionnelles et de navigation sont conservées pendant deux ans à compter de votre dernière activité sur la plateforme. Passé ce délai, les comptes inactifs seront supprimés.</p>
+
+                    <h2>Article 3 – Objet de la Plateforme</h2>
+                    <p>La plateforme est un service en expérimentation conçu pour aider les enseignants de sciences à créer et personnaliser des cours en combinant leurs propres ressources avec celles fournies par Universcience. Elle intègre des outils d’intelligence artificielle pour proposer des contenus pédagogiques adaptés et automatiser certaines tâches, telles que la création de quiz.</p>
+
+                    <h2>Article 4 – Fonctionnalités</h2>
+                    <h3>4.1 Création et Personnalisation de Cours</h3>
+                    <p>Les enseignants peuvent créer des cours en intégrant divers formats de contenu, comme des vidéos, documents, images, et jeux éducatifs, fournis par Universcience.</p>
+
+                    <h3>4.2 Utilisation de l’Intelligence Artificielle (IA)</h3>
+                    <p>L’IA est utilisée pour proposer des quiz automatiques basés sur le contenu des chapitres créés et pour l’indexation des contenus disponibles sur la plateforme, permettant une recherche plus précise et pertinente.</p>
+
+                    <h3>4.3 Téléchargement et Utilisation des Contenus</h3>
+                    <p>Les enseignants peuvent télécharger les cours en PDF.</p>
+
+                    <h3>4.4 Recherche de Contenus</h3>
+                    <p>Le moteur de recherche sémantique intégré permet aux enseignants de trouver rapidement du contenu pertinent à l’aide de mots-clés ou d’expressions sémantiques, en filtrant par type de contenu (vidéos, documents, images, etc.).</p>
+
+                    <h3>4.5 Gestion des Contenus Externes</h3>
+                    <p>Les contenus externes intégrés via des liens ou iframes peuvent être modifiés directement sur la plateforme, permettant aux enseignants de les adapter à leurs besoins pédagogiques spécifiques.</p>
+
+                    <h3>4.6 Assistance et Support</h3>
+                    <p>Chaque enseignant dispose d’un espace personnel pour accéder à ses cours favoris et créés. Un support est disponible pour les utilisateurs via l’adresse électronique : <a href="mailto:science-infuse@universcience.fr">science-infuse@universcience.fr</a>.</p>
+
+                    <h3>4.7 Catalogue de cours clé en main</h3>
+                    <p>La plateforme propose un catalogue de cours clé en main conçus par Universcience et ses partenaires. Ces cours intègrent des contenus pédagogiques variés et adaptés aux programmes scolaires. Les enseignants peuvent utiliser ces cours tels quels ou les adapter à leurs besoins.</p>
+
+                    <h3>4.8 Edition de cours</h3>
+                    <p>Les enseignants ont la possibilité de modifier les cours clé en main, ou de créer leurs propres cours à partir des contenus disponibles sur la plateforme.</p>
+
+                    <h2>Article 5 – Responsabilités</h2>
+                    <h3>5.1 L’Éditeur de la Plateforme</h3>
+                    <p>Universcience, en tant qu'éditeur de la plateforme, est responsable de la mise à disposition de la plateforme et de la qualité des contenus proposés. Il s'engage à garantir la fiabilité scientifique des informations et à assurer un support adéquat aux utilisateurs.</p>
+
+                    <h3>5.2 Gestion des données</h3>
+                    <p>Le ministère de la Culture est le responsable du traitement des données à caractère personnel collectées via la plateforme Science Infuse, au sens du Règlement Général sur la Protection des Données (RGPD). Le ministère de la Culture est responsable de la gestion technique de la plateforme, incluant le développement, la maintenance, la sécurité du site web, ainsi que la collecte, le stockage et l'utilisation des données personnelles des utilisateurs.</p>
+
+                    <h3>5.3 Les utilisateurs</h3>
+                    <p>Les enseignants s'engagent à utiliser la plateforme conformément aux présentes CGU. Ils sont responsables de l'exactitude des informations fournies lors de leur inscription et de l'utilisation qu'ils font des contenus de la plateforme. Ils s'engagent à ne pas commercialiser les données reçues et à respecter les droits d'auteur relatifs aux contenus fournis par la plateforme.</p>
+
+                    <h2>Article 6 – Propriété Intellectuelle</h2>
+                    <h3>6.1 Contenu d'Universcience</h3>
+                    <p>Les contenus fournis par Universcience et ses partenaires (vidéos, images, jeux pédagogiques, etc.) sont protégés par le droit d'auteur et restent la propriété d'Universcience et de ses partenaires. Les enseignants bénéficient d'une licence d'utilisation non exclusive de ces contenus dans le cadre de la plateforme, leur permettant d'utiliser, de modifier et de partager ces contenus uniquement à des fins pédagogiques et non commerciales, et sous réserve de mentionner les auteurs originaux.</p>
+
+                    <h3>6.2 Cours créés par les enseignants</h3>
+                    <p>Les enseignants conservent les droits d'auteur sur les cours originaux qu'ils créent et publient sur la plateforme. En publiant un cours, ils accordent à Universscience une utilisation non exclusive, gratuite, mondiale et perpétuelle pour utiliser, reproduire, modifier, adapter, publier, traduire, distribuer et afficher ce cours sur la plateforme et les autres environnements numériques d’Universcience, à des fins pédagogiques, commerciales ou non commerciales.</p>
+
+                    <h2>Article 7 – Confidentialité des Données</h2>
+                    <p>Le traitement des données à caractère personnel collectées via Science Infuse est réalisé sous la responsabilité du ministère de la Culture.</p>
+                    <p>La plateforme collecte les données suivantes :</p>
+                    <ul>
+                        <li>Données de contact : Prénom, nom, adresse électronique professionnelle, nom de l’établissement scolaire, académie de rattachement.</li>
+                        <li>Données professionnelles : Niveaux d’enseignement, matière enseignée.</li>
+                        <li>Données de navigation : Statistiques d’utilisation via Matomo, sans collecte de données identifiantes (pages vues, sessions, requêtes issues du moteur de recherche, etc.).</li>
+                    </ul>
+
+                    <h2>Article 8 – Mise à Jour des CGU</h2>
+                    <p>Les termes des CGU doivent être acceptés au moment de la connexion. Toute modification des CGU nécessitera le consentement des utilisateurs pour continuer à utiliser la plateforme.</p>
+
+                    <h2>Article 9 – Résiliation et Désinscription</h2>
+                    <p>Les utilisateurs peuvent se désinscrire à tout moment en envoyant un mail à <a href="mailto:science-infuse@universcience.fr">science-infuse@universcience.fr</a>. Les contenus déjà publiés sur la plateforme restent la propriété d’Universcience et peuvent continuer à être utilisés.</p>
+
+                    <h2>Article 10 – Contacts</h2>
+                    <p>Pour toute question ou demande, les utilisateurs peuvent contacter Universcience à l’adresse électronique suivante : <a href="mailto:science-infuse@universcience.fr">science-infuse@universcience.fr</a>.</p>
+
+                    <h2>Article 11 – Utilisation de l’Intelligence Artificielle (IA)</h2>
+                    <h3>11.1 Finalités</h3>
+                    <p>La plateforme Science Infuse utilise l’IA pour faciliter la création et la personnalisation de cours, notamment pour :</p>
+                    <ul>
+                        <li>Suggérer du contenu pertinent en fonction du sujet du cours et du niveau des élèves.</li>
+                        <li>Adapter le niveau de difficulté des exercices et des quiz.</li>
+                        <li>Générer automatiquement des quiz à partir du contenu du cours.</li>
+                        <li>Indexer et analyser les contenus pédagogiques provenant d'Universcience et de ses partenaires.</li>
+                        <li>Traduire automatiquement des descriptions d'images.</li>
+                        <li>Analyser la structure des documents PDF.</li>
+                        <li>Améliorer la recherche sémantique.</li>
+                    </ul>
+
+                    <h3>11.2 Transparence</h3>
+                    <p>Universcience s'engage à faire preuve de transparence quant à l'utilisation de l'IA sur la plateforme. Les enseignants sont informés des fonctionnalités qui utilisent l'IA et de la manière dont celle-ci est utilisée pour traiter leurs données.</p>
+
+
+
+                </div >
+            </div >
+        </div >
+    )
+}
+
+export default CGU;
