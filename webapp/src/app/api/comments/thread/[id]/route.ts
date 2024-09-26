4fce9c6d542fa378a70a1e6a50fd54af695621f8
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-26 10:30:57 +0200

    fix data saved as string instead of json

--- webapp/src/app/api/comments/thread/[id]/route.ts
+++ webapp/src/app/api/comments/thread/[id]/route.ts
@@ -23,7 +23,7 @@ export async function POST(
     });
 
     if (!chapter) {
-      return NextResponse.json({ error: 'Chapter not found' }, { status: 404 });
+      return NextResponse.json({ error: 'Thread not found' }, { status: 404 });
     }
 
     const newMessage = await prisma.comment.create({
