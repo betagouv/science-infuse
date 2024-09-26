1bbf7a24f3bd1e4884d3638215c601e4249f5c3d
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-26 13:16:23 +0200

    add pictures to venir-a-univers-science

--- webapp/src/components/SIFooter.tsx
+++ webapp/src/components/SIFooter.tsx
@@ -3,7 +3,7 @@ export default () => (
         <div className="fr-col-12 fr-col-md-8 main-content-item">
 
             <div className="w-full flex flex-row justify-evenly items-center py-8 ">
-                <a href="/venir-a-univers-science" className="m-0 text-xs font-bold text-[#161616]">
+                <a href="/venir-a-universcience" className="m-0 text-xs font-bold text-[#161616]">
                     Venir à la Cité des sciences et de l’industrie et au Palais de la découverte
                 </a>
                 <a href="/conditions-generales-d-utilisation" className="m-0 text-xs font-bold text-[#161616]">
