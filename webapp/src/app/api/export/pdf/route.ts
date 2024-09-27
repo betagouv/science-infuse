4a5183e1d3fd536cbe2b098732b7617c0c991911
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-27 14:50:04 +0200

    improve pdf export

--- webapp/src/app/api/export/pdf/route.ts
+++ webapp/src/app/api/export/pdf/route.ts
@@ -6,7 +6,9 @@ export const dynamic = 'force-dynamic'
 export async function POST(request: NextRequest) {
     const { html } = await request.json();
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     const pdfBuffer = await page.pdf({ format: 'A4' });
