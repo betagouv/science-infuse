33e0898d19f013c03e0f7bc9da7b293fc545c73f
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-27 15:46:06 +0200

    improve course editor side scroll

--- webapp/src/course_editor/components/CourseSettings/index.tsx
+++ webapp/src/course_editor/components/CourseSettings/index.tsx
@@ -6,7 +6,7 @@ import ShareToScienceInfuse from "./ShareToScienceInfuse";
 const CourseSettings = (props: { editor: Editor }) => {
     return <div className="sticky top-8 h-full bg-white z-[1] flex flex-col gap-8 w-[300px]">
         <CourseInformations editor={props.editor} />
-        <div className="flex flex-col gap-4 sticky bottom-4 w-full items-center justify-center z-[2] bg-white mt-auto">
+        <div className="flex flex-col gap-4 sticky bottom-0 pb-4 w-full items-center justify-center z-[2] bg-white mt-auto">
             <ExportToPdf editor={props.editor} />
             {props.editor.isEditable && <ShareToScienceInfuse editor={props.editor} />}
         </div>
