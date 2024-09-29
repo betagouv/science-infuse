a72d19b0e1c0684f0503718722aea9f6ec62bd42
Erwan BOEHM <erwan.boehm@gmail.com>
2024-09-29 10:26:44 +0200

    improve block embedding context with quiz and chapter title

--- webapp/src/course_editor/extensions/SaveCourse/saveToDb.ts
+++ webapp/src/course_editor/extensions/SaveCourse/saveToDb.ts
@@ -3,31 +3,31 @@ import { TSeverity } from '@/app/SnackBarProvider';
 import { apiClient } from '@/lib/api-client';
 import { EducationLevel, KeyIdea, Skill } from '@prisma/client';
 
-export const saveBlocks = async (chapterId: string, editorContent: JSONContent) => {
-    const blocks = editorContent.content?.filter(element => element.type === "courseBlock")
-    console.log("BLOCKS", blocks, editorContent)
-    if (blocks && blocks.length > 0) {
-        for (let i = 0; i < blocks.length; i++) {
-            const block = blocks[i]
-            if (block.attrs && block.attrs.id && block.content) {
-                const blockId = block.attrs.id;
-                // const blockKeyIdeas = (block.attrs.keyIdeas || []).map((b: KeyIdea) => b.id);
-                const blockTitle = block.attrs?.title || ""
-                console.log("BLOCK TITLE", blockTitle, block.content);
+// export const saveBlocks = async (chapterId: string, editorContent: JSONContent) => {
+//     const blocks = editorContent.content?.filter(element => element.type === "courseBlock")
+//     console.log("BLOCKS", blocks, editorContent)
+//     if (blocks && blocks.length > 0) {
+//         for (let i = 0; i < blocks.length; i++) {
+//             const block = blocks[i]
+//             if (block.attrs && block.attrs.id && block.content) {
+//                 const blockId = block.attrs.id;
+//                 // const blockKeyIdeas = (block.attrs.keyIdeas || []).map((b: KeyIdea) => b.id);
+//                 const blockTitle = block.attrs?.title || ""
+//                 console.log("BLOCK TITLE", blockTitle, block.content);
 
-                try {
-                    const saved = await apiClient.updateBlock(chapterId, blockId, blockTitle, block.content)
-                    if (!saved) {
-                        return false;
-                    }
-                } catch (error) {
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
+//                 try {
+//                     const saved = await apiClient.updateBlock(chapterId, blockId, blockTitle, block.content)
+//                     if (!saved) {
+//                         return false;
+//                     }
+//                 } catch (error) {
+//                     return false;
+//                 }
+//             }
+//         }
+//     }
+//     return true;
+// }
 
 interface HandleSaveResponse {
     message: string,
@@ -45,15 +45,15 @@ export const handleSave = async (editor: Editor, chapterId: string, title: strin
     } catch (error) {
         saveChapterOk = false;
     }
-    const saveBlockOk = await saveBlocks(chapterId, editor.getJSON())
+    // const saveBlockOk = await saveBlocks(chapterId, editor.getJSON())
 
     if (!saveChapterOk) {
         return { message: "Erreur lors de la sauvegarde du chapitre", severity: "error" }
     }
 
-    if (!saveBlockOk) {
-        return { message: "Erreur lors de la sauvegarde d'un bloc.", severity: "error" }
-    }
+    // if (!saveBlockOk) {
+    //     return { message: "Erreur lors de la sauvegarde d'un bloc.", severity: "error" }
+    // }
 
     return { message: "Chapitre sauvegardé.", severity: "success" }
 };
