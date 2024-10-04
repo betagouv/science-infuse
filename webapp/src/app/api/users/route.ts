6032195e364452bc926d4d17353748f745b96258
Erwan BOEHM <erwan.boehm@gmail.com>
2024-10-04 18:11:41 +0200

    admin pannel users see and update

--- webapp/src/app/api/users/route.ts
+++ webapp/src/app/api/users/route.ts
@@ -5,40 +5,32 @@ import { authOptions } from '../auth/[...nextauth]/authOptions';
 import { userIs, withAccessControl } from '../accessControl';
 import { UserRoles } from '@prisma/client';
 
-export const GET = async (request: NextRequest, { params, user }: { params: { id: string }, user: User }) => {
-    try {
-        const users = await prisma.user.findMany({
-            select: {
-                id: true,
-                firstName: true,
-                job: true,
-                email: true,
-                roles: true,
-                school: true,
-                lastName: true,
-                emailVerified: true,
-                image: true,
-                academyId: true,
-                educationLevels: true,
-                schoolSubjects: true,
-            }
-        });
-        return NextResponse.json(users);
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
-};
 
-// export const GET = withAccessControl(
-//     { allowedRoles: ['ADMIN'] },
-//     async (request: NextRequest, { params, user }: { params: { id: string }, user: User }) => {
-//         try {
-//             const users = await prisma.user.findMany();
-//             return NextResponse.json(users);
-//         } catch (error) {
-//             console.error('Error fetching users:', error);
-//             return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-//         }
-//     },
-// )
+export const GET = withAccessControl(
+    { allowedRoles: ['ADMIN'] },
+    async (request: NextRequest, { params, user }: { params: { id: string }, user: User }) => {
+        try {
+            const users = await prisma.user.findMany({
+                select: {
+                    id: true,
+                    firstName: true,
+                    job: true,
+                    email: true,
+                    roles: true,
+                    school: true,
+                    lastName: true,
+                    emailVerified: true,
+                    image: true,
+                    academyId: true,
+                    educationLevels: true,
+                    schoolSubjects: true,
+                }
+            });
+
+            return NextResponse.json(users);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+        }
+    },
+)
