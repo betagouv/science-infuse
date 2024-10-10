5e86ed65e881c470fa96219d62b4a94e40281f38
Erwan BOEHM <erwan.boehm@gmail.com>
2024-10-10 11:05:00 +0200

    documentation at root

--- documentation/app/(home)/layout.tsx
+++ documentation/app/(home)/layout.tsx
@@ -1,11 +1,12 @@
+import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/home-layout';
 import { baseOptions } from '../layout.config';
+import { source } from '@/app/source';
 
-export default function Layout({
-  children,
-}: {
-  children: ReactNode;
-}): React.ReactElement {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <DocsLayout tree={source.pageTree} {...baseOptions}>
+      {children}
+    </DocsLayout>
+  );
 }
