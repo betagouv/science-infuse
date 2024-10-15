7c383188ef00d6345ddb4f579b56c1ed3ec85bd1
Erwan BOEHM <erwan.boehm@gmail.com>
2024-10-15 18:56:21 +0200

    add file indexing queue, view processing queue, inspect and desindex document

---
+++ webapp/src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import AdminWrapper from './AdminWrapper';
+
+const AdminListUsers = () => {
+
+    return (
+        <AdminWrapper>
+            <p className='w-full text-center'>Veuillez sélectionner une page dans le menu de navigation à gauche</p>
+        </AdminWrapper>
+    )
+}
+
+export default AdminListUsers;
