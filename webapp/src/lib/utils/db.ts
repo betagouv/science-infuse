28d5aa5e7407cdf5cdd849b67471547ff349ddf6
Erwan BOEHM <erwan.boehm@gmail.com>
2024-10-17 20:59:12 +0200

    manually report search to matomo + display number of videos (not nbr of chunks)

--- webapp/src/lib/utils/db.ts
+++ webapp/src/lib/utils/db.ts
@@ -1,5 +1,8 @@
 import { ChapterWithBlock, UserFull } from "@/types/api";
+import { DocumentChunk, Document } from "@prisma/client";
+import { v4 as uuidv4 } from 'uuid'; // Make sure to import the uuid library
 import prisma from "../prisma";
+import { getEmbeddings } from "./getEmbeddings";
 
 export const getChaptersWithBlocks = async (userId: string): Promise<ChapterWithBlock[]> => {
     return await prisma.chapter.findMany({
@@ -43,4 +46,46 @@ export const getUserFull = async (userId: string) => {
         }
     });
     return user;
+}
+
+export const insertDocument = async (document: Document, chunks: (DocumentChunk & { document: any, metadata: any })[], hash: string): Promise<string> => {
+
+    const createdDocument = await prisma.document.create({
+        data: {
+            ...document,
+            fileHash: hash,
+        },
+    });
+
+    // create chunks (and  metadatas) and link them to the new document
+    await Promise.all(chunks.map(async ({ document, metadata, id, ...chunk }) => {
+        const chunkId = uuidv4();
+        const textEmbedding = await getEmbeddings(chunk.text);
+        // insert using queryRaw since vector is unsupported as a type.
+        await prisma.$queryRaw`
+        INSERT INTO "DocumentChunk" (
+          id, text, "textEmbedding", test, title, "mediaType", "documentId"
+        ) VALUES (
+          ${chunkId}::uuid, 
+          ${chunk.text}, 
+          ${textEmbedding}::vector, 
+          ${chunk.test || 'test'}, 
+          ${chunk.title}, 
+          ${chunk.mediaType}, 
+          ${createdDocument.id}::uuid
+        )
+      `;
+
+        if (metadata) {
+            await prisma.documentChunkMeta.create({
+                data: {
+                    id: uuidv4(),
+                    ...metadata,
+                    documentChunkId: chunkId,
+                },
+            });
+        }
+
+    }));
+    return createdDocument.id;
 }
