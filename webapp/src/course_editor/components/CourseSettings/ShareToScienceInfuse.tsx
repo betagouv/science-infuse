87bb2d19bfac710d41214dfc856f8cd277377f04
Erwan BOEHM <erwan.boehm@gmail.com>
2024-11-05 12:54:05 +0100

    fix change course status + search PUBLISHED course (no more review)

--- webapp/src/course_editor/components/CourseSettings/ShareToScienceInfuse.tsx
+++ webapp/src/course_editor/components/CourseSettings/ShareToScienceInfuse.tsx
@@ -4,16 +4,21 @@ import Button from '@codegouvfr/react-dsfr/Button';
 import { CircularProgress, Tooltip } from '@mui/material';
 import { ChapterStatus } from '@prisma/client';
 import { Editor } from '@tiptap/react';
+import { ValueOf } from 'next/dist/shared/lib/constants';
 import { useEffect, useState } from 'react';
 
 const ShareToScienceInfuse = (props: { chapter: ChapterWithoutBlocks }) => {
     const [chapterStatus, setChapterStatus] = useState<ChapterStatus | undefined>(props.chapter?.status);
     const [loadingMessage, setLoadingMessage] = useState("");
     const chapter = props.chapter;
+    const expectedStatus = [ChapterStatus.REVIEW, ChapterStatus.PUBLISHED].includes(chapterStatus as any) ? ChapterStatus.DRAFT : ChapterStatus.REVIEW;
 
     if (!chapter || !chapterStatus) return;
     return (
-        <Tooltip title={chapterStatus === ChapterStatus.REVIEW && <span>Votre cours est en attente de revue.<br />Cliquez pour annuler la demande.</span>}>
+        <Tooltip title={
+            chapterStatus === ChapterStatus.REVIEW && <span>Votre cours est en attente de revue.<br />Cliquez pour annuler la demande.</span> ||
+            chapterStatus === ChapterStatus.PUBLISHED && <span>Votre cours est en ligne.<br />Cliquez pour annuler la demande.</span>
+        }>
             <Button
                 iconId="fr-icon-group-fill"
                 iconPosition="right"
@@ -23,10 +28,9 @@ const ShareToScienceInfuse = (props: { chapter: ChapterWithoutBlocks }) => {
                 // priority={!loadingMessage || chapterStatus == ChapterStatus.REVIEW ? "secondary" : "primary"}
                 onClick={async () => {
                     try {
-                        const expectedStatus = chapterStatus == ChapterStatus.REVIEW ? ChapterStatus.DRAFT : ChapterStatus.REVIEW;
-                        expectedStatus == ChapterStatus.REVIEW && setLoadingMessage("Envoie de la demande");
-                        expectedStatus == ChapterStatus.DRAFT && setLoadingMessage("Annulation de la demande");
-                        console.log("expectedStatus", expectedStatus)
+                        expectedStatus === ChapterStatus.REVIEW && setLoadingMessage("Envoie de la demande");
+                        expectedStatus === ChapterStatus.DRAFT && setLoadingMessage("Annulation de la demande");
+                        console.log("expectedStatus", expectedStatus);
                         await apiClient.updateChapter(chapter.id, { status: expectedStatus });
                         setTimeout(() => {
                             setChapterStatus(expectedStatus);
@@ -39,7 +43,9 @@ const ShareToScienceInfuse = (props: { chapter: ChapterWithoutBlocks }) => {
 
                 {loadingMessage && <>{loadingMessage}  <CircularProgress className='ml-2' size={16} sx={{ color: 'black' }} /></>}
                 {!loadingMessage && (
-                    chapterStatus == ChapterStatus.REVIEW ? "Demande envoyée" : "Partager à Science Infuse"
+                    chapterStatus == ChapterStatus.REVIEW && "Demande envoyée" ||
+                    chapterStatus == ChapterStatus.DRAFT && "Partager à Science Infuse" ||
+                    chapterStatus == ChapterStatus.PUBLISHED && "En ligne"
                 )}
             </Button>
         </Tooltip >
