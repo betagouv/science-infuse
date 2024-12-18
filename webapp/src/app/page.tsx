c32701d80528a62c850b7faaf618940baf1eaf36
Erwan BOEHM <erwan.boehm@gmail.com>
2024-12-18 16:19:26 +0100

    quick UX fixes

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -16,14 +16,9 @@ export default function Page() {
         <>
             <MobileNotSupportedModal />
             <div className="w-full fr-grid-row fr-grid-row--gutters fr-grid-row--center">
-                {user ?
                     <div className="fr-col-12 fr-col-md-8 main-content-item my-24">
                         <MainSearch />
                     </div> :
-                    <div className="fr-col-12 fr-col-md-6 fr-col-lg-4 main-content-item my-16">
-                        <Login />
-                    </div>
-                }
             </div>
 
             <Section1 />
