fe0b04c98a390926cdc522836234d1bfbbcca9d8
Erwan BOEHM <erwan.boehm@gmail.com>
2025-07-01 21:11:06 +0200

    add matomo

--- webapp/src/components/MatomoAnalytics.tsx
+++ webapp/src/components/MatomoAnalytics.tsx
@@ -1,25 +1,35 @@
+'use client';
 
 import Script from 'next/script';
 import React from 'react';
 
-const MatomoAnalytics = () => (
-  <>
-    <Script id="matomo-analytics" strategy="lazyOnload">
-{`
- var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://stats.beta.gouv.fr/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '145']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-`}
-    </Script>
-  </>
-);
+const MatomoAnalytics = () => {
+  const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
+  const matomoSiteId = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+  if (!matomoUrl || !matomoSiteId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script id="matomo-analytics" strategy="lazyOnload">
+        {`
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u = "${matomoUrl}";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '${matomoSiteId}']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        `}
+      </Script>
+    </>
+  );
+};
 
 export default MatomoAnalytics;
